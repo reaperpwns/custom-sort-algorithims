e10142b5950d7e4b27845908084987cc7c05e6eb
Sohini Bonthala <sohinibonthala@Sohinis-MacBook-Pro.local>
2022-10-04 13:37:18 -0700

    secon commit

--- custom-sorts.js
+++ custom-sorts.js
@@ -16,10 +16,28 @@ function oddEvenSort(arr) {
 
 function validAnagrams(s, t) {
   // Your code here
+  return s.split('').sort().join('') === t.split('').sort().join('')
 }
 
 function reverseBaseSort(arr) {
   // Your code here
+  // let sorted = arr.sort((a,b)=>{
+  //   if(a > 100 || b > 100) return a - b;
+  //   if(a > 10 || b > 10) return a - b;
+  //   else {
+  //     return a - b
+  //   }
+  // })
+  // return sorted;
+
+  return arr.sort((a,b)=>{
+    if(a.toString().length >= b.toString().length){
+      if(a.toString().length === b.toString().length){
+        return a - b
+      }
+      return b - a
+    }
+  })
 }
 
 function frequencySort(arr) {
